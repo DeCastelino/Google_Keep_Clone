059865d5c81f609d7e5aab976f03efe5fe31edb3
Daryll Castelino <s3825140@student.rmit.edu.au>
2022-03-31 21:58:19 +1100

    Revert "Added Search functionality to label search and system wide search."
    This reverts commit f25552033d7f05c313d725630a4ad74764151952.

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,5 +1,20 @@
+import { useState, useEffect } from "react";
+
+import FilteredLayout from "./FilteredLayout";
+import Navbar from "./Navbar";
+
 const Search = () => {
-    return <></>;
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const selectedLabel = urlParams.get("value");
+    console.log(selectedLabel);
+
+    return (
+        <>
+            <Navbar />
+            <FilteredLayout selectedLabel={selectedLabel} />
+        </>
+    );
 };
 
 export default Search;
